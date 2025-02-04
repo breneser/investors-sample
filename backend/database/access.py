b0293f844e80ecc09e0ba31252ba82c7f6955f7d
Bren <bren@androsaxon.co.uk>
2025-02-04 12:09:01 +0000

    Add commitments endpoint
    - Add commitments database accessor

--- backend/database/access.py
+++ backend/database/access.py
@@ -26,3 +26,25 @@ class InvestorAccessor:
                 select(models.Investor).limit(limit).offset(offset)
             ).all()
         ]
+
+
+class CommitmentAccessor:
+    """Commitment database access object
+
+    Attributes:
+        session: database session
+    """
+
+    def __init__(self, session: Session):
+        self.session = session
+
+    def get_offset(self, offset: int = 0, limit: int = 10) -> list[schemas.Commitment]:
+        """
+        Read and return list of Commitments from the db with given limit
+        """
+        return [
+            schemas.Commitment.model_validate(commitment)
+            for commitment in self.session.scalars(
+                select(models.Commitment).limit(limit).offset(offset)
+            )
+        ]
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 from typing import Annotated
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
 from database import create_tables, open_session
-from database.access import InvestorAccessor
+from database.access import CommitmentAccessor, InvestorAccessor
 
 app = FastAPI()
 
@@ -19,7 +19,26 @@ def on_startup():
 @app.get("/investors")
 def get_investors(session: Session, page: int = 1):
     # 10 items per page by default, TODO: make configurable?
-    return InvestorAccessor(session).get_offset(
+    page = page if page > 0 else 1
+    investors = InvestorAccessor(session).get_offset(
         offset=(page - 1) * 10,
         limit=10,
     )
+
+    if not investors:
+        raise HTTPException(status_code=404, detail="Page not found")
+
+    return investors
+
+
+@app.get("/investors/{id}/commitments")
+def get_commitments(session: Session, page: int = 1):
+    # 10 items per page by default, TODO: make configurable?
+    page = page if page > 0 else 1
+    commitments = CommitmentAccessor(session).get_offset(
+        offset=(page - 1) * 10, limit=10
+    )
+    if not commitments:
+        raise HTTPException(status_code=404, detail="Page not found")
+
+    return commitments
