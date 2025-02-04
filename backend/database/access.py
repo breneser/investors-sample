ddf45b573c7da60f5a9922647a74c1618c27deae
Bren <bren@androsaxon.co.uk>
2025-02-04 12:05:40 +0000

    Add investors endpoint
    - Add db accessor for investor

---
+++ backend/database/access.py
@@ -0,0 +1,28 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+import schemas
+
+from . import models
+
+
+class InvestorAccessor:
+    """Investor database access object
+
+    Attributes:
+        session: database session
+    """
+
+    def __init__(self, session: Session):
+        self.session = session
+
+    def get_offset(self, offset: int = 0, limit: int = 10) -> list[schemas.Investor]:
+        """
+        Read and return list of Investors from the db with given limit
+        """
+        return [
+            schemas.Investor.model_validate(investor)
+            for investor in self.session.scalars(
+                select(models.Investor).limit(limit).offset(offset)
+            ).all()
+        ]
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from typing import Annotated
 
-from database import create_tables, open_session
 from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
+
+from database import create_tables, open_session
+from database.access import InvestorAccessor
 
 app = FastAPI()
 
@@ -12,3 +14,12 @@ Session = Annotated[Session, Depends(open_session)]
 @app.on_event("startup")
 def on_startup():
     create_tables()
+
+
+@app.get("/investors")
+def get_investors(session: Session, page: int = 1):
+    # 10 items per page by default, TODO: make configurable?
+    return InvestorAccessor(session).get_offset(
+        offset=(page - 1) * 10,
+        limit=10,
+    )
